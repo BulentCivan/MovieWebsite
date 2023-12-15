3f231ad1bb5506d82dcc6da0050f44f6a0f1bf79
BulentCivan <bulentcivan7@gmail.com>
2023-12-15 17:45:41 +0300

    Director entity created

--- MovieWebsite/src/main/java/com/example/MovieWebsite/entities/Director.java
+++ MovieWebsite/src/main/java/com/example/MovieWebsite/entities/Director.java
@@ -1,4 +1,12 @@
 package com.example.MovieWebsite.entities;
 
+import jakarta.persistence.Id;
+
 public class Director {
+    @Id
+    Long DirectorId;
+    String name;
+    String surname;
+    int age;
+    String Movies;
 }
